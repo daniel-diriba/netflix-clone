4ca5e9dd84d4aa307685a6c955a1789c0db96bcd
daniel-diriba <danieldiriba76@gmail.com>
2024-11-10 00:06:28 +0300

    Initial commit of netflix-clone project

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjdhNjc3YTEwYTNjYTJmMmYyMDdlNTdkMzllOTMzYiIsIm5iZiI6MTczMTE4MjczNC40NzUzODU0LCJzdWIiOiI2NzJmYmQ2NjE3NzBiMDM1MWRlMzhmYzQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.c78vtqd2NWlJ1Mm7mb-pjXtPTU08yOvQeM1H_MYLID0",
+    },
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
