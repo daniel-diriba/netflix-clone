f34b2ce2dd39ad5e32b5a6162ad2ba8bddacdf99
daniel-diriba <danieldiriba76@gmail.com>
2024-11-10 12:00:01 +0300

    second commit of netflix-clone project

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Player.css";
-
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 const Player = () => {
-  return <div className="player"></div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjdhNjc3YTEwYTNjYTJmMmYyMDdlNTdkMzllOTMzYiIsIm5iZiI6MTczMTE4MjczNC40NzUzODU0LCJzdWIiOiI2NzJmYmQ2NjE3NzBiMDM1MWRlMzhmYzQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.c78vtqd2NWlJ1Mm7mb-pjXtPTU08yOvQeM1H_MYLID0",
+    },
+  };
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
